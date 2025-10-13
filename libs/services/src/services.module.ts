cd65af13bdb710fc525e3d65f1a90137a8e7839a
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-10-13 12:09:05 +0300

    move pinata config and fix services module

--- libs/services/src/services.module.ts
+++ libs/services/src/services.module.ts
@@ -1,6 +1,9 @@
 import { DynamicModuleUtils } from '@libs/common';
 import { DatabaseModule } from '@libs/database';
+import { ApiModule } from '@multiversx/sdk-nestjs-http';
+import { MetricsModule, MetricsService } from '@multiversx/sdk-nestjs-monitoring';
 import { Global, Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ExampleService } from './example/example.service';
 import { PinataService } from './pinata';
 import { TaskService } from './task';
@@ -11,8 +14,33 @@ import { VerifierService } from './verifier';
 @Global()
 @Module({
   imports: [
+    MetricsModule,
+    ApiModule.forRootAsync({
+      imports: [MetricsModule],
+      useFactory: (metricsService: MetricsService) => ({
+        axiosTimeout: 10000,
+        retryAttempts: 3,
+        retryDelay: 1000,
+        metrics: metricsService,
+        useKeepAliveAgent: true,
+        serverTimeout: 10000,
+        logConnectionKeepAlive: false,
+        useKeepAliveHeader: true,
+      }),
+      inject: [MetricsService],
+    }),
     DatabaseModule,
     DynamicModuleUtils.getCachingModule(),
+    ClientsModule.register([
+      {
+        name: 'PUBSUB_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          host: process.env.REDIS_HOST || 'localhost',
+          port: parseInt(process.env.REDIS_PORT || '6379'),
+        },
+      },
+    ]),
   ],
   providers: [
     TokenService,
